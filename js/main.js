70c3059e2c95f163a82314d98e6f0f5fe86b09ef
Juan Carlos Serrudo <49172116+serrudojc@users.noreply.github.com>
2020-01-21 21:01:09 -0300

    clase 8
    Objetos y Json

--- js/main.js
+++ js/main.js
@@ -1,72 +1,99 @@
-
-
-function validarNombre(nombre){
-    if(nombre.length === 0){
+function validarNombre(nombre) {
+    if (nombre.length === 0) {
         return "Este campo debe tener al menos 1 caracter";
     }
-    if(nombre.length >= 50){
+
+    if (nombre.length >= 50) {
         return "Este campo debe tener menos de 50 caracteres";
     }
 
-    if(!/^[a-z]+$/i.test(nombre)){
-        return('El campo nombre sólo acepta letras')
+    if (!/^[a-z]+$/i.test(nombre)) {
+        return "El campo nombre sólo acepta letras";
     }
 
     //si no, devuelvo string vacio (sin error)
     return "";
 }
 
-function validarCiudad(ciudad){
-    if(ciudad.length === 0){ // ciudad === ''
-        return 'El campo ciudad no puede estar vacio';
+function validarCiudad(ciudad) {
+    if (ciudad.length === 0) {
+        // ciudad === ''
+        return "El campo ciudad no puede estar vacio";
     }
-    return '';
+    return "";
 }
 
-function validarDescripcionRegalo(descripcionRegalo){
-    if(descripcionRegalo.length >= 100){
-        return 'El campo descripcion es muy largo';
-    }else if(descripcionRegalo.length === 0){
-        return 'El campo descripcion no puede estar vacio';
+function validarDescripcionRegalo(descripcionRegalo) {
+    if (descripcionRegalo.length >= 100) {
+        return "El campo descripcion es muy largo";
+    } else if (descripcionRegalo.length === 0) {
+        return "El campo descripcion no puede estar vacio";
         //si no agrego el i, sólo toma minusculas in case sentive.
-    }else if(!/^[a-z0-9]+$/i.test(descripcionRegalo)){
+    } else if (!/^[a-z0-9]+$/i.test(descripcionRegalo)) {
         return "El campo descripcion solo puede tener numeros y letras";
-    }else{
-        return '';
-    }    
+    } else {
+        return "";
+    }
 }
 
-function validarFormulario(event){
-    const $form = document.querySelector('#carta-a-santa');
-    const errorNombre = validarNombre($form.nombre.value);
+function validarFormulario(event) {
+    const $form = document.querySelector("#carta-a-santa");
 
-    //const nombre = $form.nombre.value;
+    const nombre = $form.nombre.value;
     //nombre = document.querySelector('[name=nombre]').value;
     //nombre = document.querySelector('#nombre').value;
-
     const ciudad = $form.ciudad.value;
-    const comportamiento = $form.comportamiento.value;
-    const descripcionRegalo = $form['descripcion-regalo'].value;
+    //const comportamiento = $form.comportamiento.value;
+    const descripcionRegalo = $form["descripcion-regalo"].value;
 
     //console.log(nombre);
     //console.log(ciudad);
     //console.log(comportamiento);
     //console.log(descripcionRegalo);
 
-    manejarErrores([errorNombre]);
+    const errorNombre = validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+    //objeto errores
+    const errores = {
+        "descripcion-regalo": errorDescripcionRegalo,
+        ciudad: errorCiudad,
+        nombre: errorNombre
+    };
+
+    console.log(errores);
+
+    const esExito = manejarErrores(errores) === 0;
+
+    if (esExito) {
+        $form.className = "oculto";
+        document.querySelector("#exito").className = "";
+    }
 
     event.preventDefault();
 }
 
-function manejarErrores(errores){
-    errorNombre = errores[0];   //nombre
+function manejarErrores(errores) {
+    const keys = Object.keys(errores);
+    const $errores = document.querySelector("#errores");
+    let cantidadErrores = 0;
 
-    if(errorNombre){
-        $form.nombre.className = "error";
-    }else{
-        $form.nombre.className = "";
-    }
+    keys.forEach(function(key) {
+        const error = errores[key];
+
+        if (error) {
+            cantidadErrores++;
+            $form[key].className = "error";
+            const $error = document.createElement("li");
+            $error.innerText = error;
+            $errores.appendChild($error);
+        } else {
+            $form[key].className = "";
+        }
+    });
+    return cantidadErrores;
 }
 
-const $form = document.querySelector('#carta-a-santa');
+const $form = document.querySelector("#carta-a-santa");
 $form.onsubmit = validarFormulario;
